981b5d6326b34c559a5c180b12f7167b96b55653
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2024-05-23 14:32:00 +1000

    fixed download url

--- undetected_chromedriver/patcher.py
+++ undetected_chromedriver/patcher.py
@@ -280,7 +280,7 @@ class Patcher(object):
             download_url = "%s/%s/%s" % (self.url_repo, str(self.version_full), zip_name)
         else:
             zip_name = zip_name.replace("_", "-", 1)
-            download_url = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/%s/%s/%s"
+            download_url = "https://storage.googleapis.com/chrome-for-testing-public/%s/%s/%s"
             download_url %= (str(self.version_full), self.platform_name, zip_name)
 
         logger.debug("downloading from %s" % download_url)
